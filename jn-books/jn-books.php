2c4431d9abf57179dda11c4d2a036b4768293f88
Johan Nordstrom <johan@digitalvillage.se>
2017-05-31 11:59:16 +0200

    More verbose solution of day 1’s workshop.

--- jn-books/jn-books.php
+++ jn-books/jn-books.php
@@ -9,8 +9,8 @@
  */
 
 function jnb_author_shortcode() {
-	$book_author = get_post_meta(get_the_ID(), 'book_author', true);
-	return $book_author;
+	return get_post_meta(get_the_ID(), 'book_author', true);
+	// return $book_author;
 }
 add_shortcode('book_author', 'jnb_author_shortcode');
 
@@ -27,15 +27,29 @@ function jnb_link_shortcode() {
 add_shortcode('book_link', 'jnb_link_shortcode');
 
 function jnb_book_info_shortcode() {
+	// alt. 1, hämta custom field 'book_author'
 	$book_author = get_post_meta(get_the_ID(), 'book_author', true);
+
+	// alt. 2, gör samma som ovan
+	$book_author = jnb_author_shortcode();
+
+	// alt. 3, gör samma som ovan
+	$book_author = do_shortcode('book_author');
+
+	// hämta custom field 'book_title'
 	$book_title = get_post_meta(get_the_ID(), 'book_title', true);
+
+	// hämta custom field 'book_link'
 	$book_link = get_post_meta(get_the_ID(), 'book_link', true);
+	if (empty($book_link)) {
+		$book_link = "(this book has no link)";
+	}
 
 	return "Author: {$book_author}<br />Title: {$book_title}<br />Link: {$book_link}";
 }
 add_shortcode('book_info', 'jnb_book_info_shortcode');
 
-function jnb_books() {
+function jnb_book_reviews_shortcode() {
 	$html = "";
 	$books = new WP_Query(['post_type' => 'book_reviews']);
 	if ($books->have_posts()) {
@@ -55,4 +69,4 @@ function jnb_books() {
 	}
 	return $html;
 }
-add_shortcode('book_reviews', 'jnb_books');
+add_shortcode('book_reviews', 'jnb_book_reviews_shortcode');
